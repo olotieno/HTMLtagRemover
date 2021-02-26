3fe26522a30138046cf9d8bfbfa3c747da77cfba
olotieno <oliverlandootieno>
2021-02-26 16:48:04 +0300

    Add project files.

---
+++ HTMLtagRemover/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.IO; 
+
+namespace HTMLtagRemover
+{
+    class Program
+    {
+        private const string InputFileName = "Problem1.html";
+        private const string Charset = "windows - 1251"; 
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            StreamReader reader = new StreamReader(InputFileName);
+            String reader; 
+
+            if(!File.Exists(InputFileName) == null)
+            {
+
+                Console.WriteLine("File" + InputFileName + "does not exist"); 
+            }
+           
+        }
+    }
+}
